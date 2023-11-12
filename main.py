2c48ef4dd6c636329b1160bc21916a62d8c11000
NIDIN V NANDAN <128630742+nidinvnandan@users.noreply.github.com>
2023-11-12 14:21:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+#importing the funtions and training from chat.py file
 from chat import response,nlp,model,data,intents,words,classes
 
 # ERROR_THRESHOLD
@@ -7,12 +8,13 @@ ERROR_THRESHOLD = 0.25
 # Streamlit App
 st.title("Chatbot")
 
-# Counter for generating unique keys
 
-user_input = st.text_input('Enter the Question')
+
+user_input = st.text_input('Enter the Question(example, machine learning,nlp,deep learning,tell me a joke etc)')
 if st.button("Submit"):
         st.text("Chatbot:")
+        #here the response function will be imported from the chat.py file which contaims the training part
         response_text = response(user_input)
-        st.text(response_text)
+        st.write(response_text)
         
     
